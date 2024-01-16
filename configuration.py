909557eb8cbf81602e2e0637b9de7f6cded410eb
DamelisQuerales <141416466+damelisquerales@users.noreply.github.com>
2024-01-16 13:17:48 +0100

    Segunda entrega Project 6
    Signed-off-by: Esparta <damelisquerales@gmail.com>

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://8d7bc63f-a641-479d-a1a6-63883b1a9d83.serverhub.tripleten-services.com"
+URL_SERVICE = "https://126eea1c-c780-47c8-bfbf-789ddfd1448a.serverhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users/"
 KITS_PATH = "/api/v1/kits"
 DOC_PATH = "/docs/"
