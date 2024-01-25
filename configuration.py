1055a57e3ab69c14ec1d3b5355b12c6b1a755e23
Esparta <damelisquerales@gmail.com>
2024-01-25 18:38:35 +0100

    Commit project 6
    Signed-off-by: Esparta <damelisquerales@gmail.com>

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://19a1b10f-e7d0-4e0f-a6b4-195bc83461c8.serverhub.tripleten-services.com"
+URL_SERVICE = "https://269952b4-d6c1-4fa8-9368-baaa519fbdf3.serverhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users/"
 KITS_PATH = "/api/v1/kits"
 DOC_PATH = "/docs/"
